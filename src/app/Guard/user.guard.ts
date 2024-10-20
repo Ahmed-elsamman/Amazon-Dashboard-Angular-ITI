0acd9ceae5c86a5d773632a43618b9db57272af0
Ahmed-elsamman <samman66512@gmail.com>
2024-10-20 19:15:53 +0300

    test1

--- src/app/Guard/user.guard.ts
+++ src/app/Guard/user.guard.ts
@@ -2,6 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 // import { UserAuthenServiceService } from '../Services/user-authen-service.service';
 import { inject } from '@angular/core';
 import { AuthService } from '../Services/aut-service.service';
+import { of } from 'rxjs';
 
 export const userGuard: CanActivateFn = (route, state) => {
   const userService = inject(AuthService);
