fd66be84718cc35d9a3425da8578cb0d7cc08e5a
Ahmed-elsamman <samman66512@gmail.com>
2024-10-24 23:19:04 +0300

    commit after marge sabry

--- src/app/Services/users/users.service.ts
+++ src/app/Services/users/users.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  createdAt: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  private readonly API_URL = `${environment.API_URL}/user`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.API_URL}`);
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.API_URL}/${id}`);
+  }
+
+  createUser(user: Partial<User>): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/register`, user);
+  }
+
+  updateUser(id: string, user: Partial<User>): Observable<User> {
+    return this.http.patch<User>(`${this.API_URL}/${id}`, user);
+  }
+
+  deleteUser(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
 }
