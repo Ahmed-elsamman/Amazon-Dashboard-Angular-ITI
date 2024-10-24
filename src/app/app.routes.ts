e60efc6898b7f4f1d95d2a7ae27529fc0ce6fde7
Ahmed-elsamman <samman66512@gmail.com>
2024-10-24 22:52:38 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import { AddUserFormComponent } from './Components/add-user-form/add-user-form.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { userGuard } from './Guard/user.guard';
+import { ProductsComponentComponent } from './Components/products-component/products-component.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -61,14 +62,20 @@ export const routes: Routes = [
       ),
     title: 'settings',
   },
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./Components/products/products-routing.module').then(
+  //       (m) => m.ProductsRoutingModule
+  //     ),
+  //   canActivate: [userGuard],
+  // },
   {
     path: 'products',
-    loadChildren: () =>
-      import('./Components/products/products-routing.module').then(
-        (m) => m.ProductsRoutingModule
-      ),
+    component: ProductsComponentComponent,
     canActivate: [userGuard],
   },
+
   {
     path: 'product-upload-form',
     component: ProductUploadFormComponent,
